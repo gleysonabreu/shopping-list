19f00035f08954ca19e5b70a4f1d6931e37fb1eb
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-08-23 20:11:26 -0300

    feat: create homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+import { Checkbox } from "@/components/checkbox";
+import { Header } from "@/components/header";
+
 export default function Home() {
   return (
-    <h1>hello world</h1>
+    <main className="max-w-3xl w-full mx-auto p-6">
+      <Header />
+
+      <div className="flex gap-3 flex-col">
+        <Checkbox
+        id="string"
+        isCheck={true}
+        productName="Maçã"
+        productType="fruit"
+        quantity={1}
+        quantityType="unit"
+        />
+        <Checkbox
+        id="string"
+        isCheck={false}
+        productName="Maçã"
+        productType="fruit"
+        quantity={1}
+        quantityType="unit"
+        />
+        <Checkbox
+        id="string"
+        isCheck={false}
+        productName="Maçã"
+        productType="fruit"
+        quantity={1}
+        quantityType="unit"
+        />
+      </div>
+    </main>
   )
 }
