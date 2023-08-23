933729d671f7f1148e74f2f6ad426afb8cd3d7e5
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-08-23 11:26:22 -0300

    add style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,51 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        product: {
+          purple: {
+            light: '#A881E6',
+            dark: '#523480',
+            normal: '#7450AC'
+          }
+        },
+        gray: {
+          100: '#FBF9FE',
+          200: '#AFABB6',
+          300: '#252529',
+          400: '#17171A',
+          500: '#111112',
+          600: '#0C0C0D'
+        },
+        feedback: {
+          success: {
+            normal: '#2F723D',
+            light: '#4E995E'
+          }
+        },
+        support: {
+          pink: { 
+            light: '#DB5BBF',
+            dark: '#251622',
+          },
+          orange: { 
+            light: '#E07B67',
+            dark: '#261A17',
+          },
+          yellow: { 
+            light: '#BB9F3A',
+            dark: '#211E12',
+          },
+          green: { 
+            light: '#8CAD51',
+            dark: '#1C2015',
+          },
+          blue: { 
+            light: '#7B94CB',
+            dark: '#1A1D23',
+          }
+        }
+      }
     },
   },
   plugins: [],
