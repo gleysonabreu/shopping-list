7bf58dbcc4741ad31c3cbd132e19af0fe35b143d
Gleyson Abreu <gleyson_datu@hotmail.com>
2023-08-25 11:15:07 -0300

    feat: add red variant to button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,19 @@
+import clsx from "clsx";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
+  variant?: 'purple' | 'red';
 }
 
-export function Button({ children, ...rest }: ButtonProps) {
+export function Button({ children, variant = 'purple', ...rest }: ButtonProps) {
   return (
     <button
     {...rest}
-    className="flex items-center justify-center gap-2 p-2 rounded-full hover:bg-product-purple-dark bg-product-purple-normal transition-all">
+    className={clsx("flex items-center justify-center gap-2 p-2 rounded-full transition-all", {
+      "hover:bg-product-purple-dark bg-product-purple-normal": variant === 'purple',
+      "hover:bg-red-500 bg-red-700": variant === 'red',
+    })}>
       {children}
     </button>
   );
